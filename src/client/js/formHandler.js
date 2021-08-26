e84b471ecc624910705f6c3a3057f95b84c8db6b
constanzamorales <constanzabmorales@gmail.com>
2021-08-26 12:44:58 -0400

    Add "No results" message and interaction

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,12 +1,15 @@
 const loader = document.getElementById('loading');
+const noResultText = document.getElementById('noResults');
 
 function handleSubmit(event) {
     event.preventDefault();
+    results.classList.remove('show');
     // Display loader when request starts.
-    loader.classList.add('display');
+    noResultText.classList.add('hide');
+    loader.classList.add('show');
 
     // Select the value in the input
-    let userURL = document.getElementById('name').value
+    let userURL = document.getElementById('url').value
     // If the URL is valid, post the data
     if (Client.checkUrl(userURL)) {
         fetch('http://localhost:8081/addData', {
@@ -20,14 +23,18 @@ function handleSubmit(event) {
         .then(res => res.json())
         // Update UI
         .then(function(res) {
+        const results = document.getElementById('results');
             console.log(res);
             // Remove loader once data is fetched.
-            loader.classList.remove('display');
+            loader.classList.remove('show');
             // Update UI
             document.getElementById('agreement').textContent = `${res.agreement} ✅`;
             document.getElementById('subjectivity').textContent = `${res.subjectivity} ✅`;
             document.getElementById('irony').textContent = `${res.irony} ✅`;
             document.getElementById('confidence').textContent = `${res.confidence}/100 ✅`;
+
+            results.classList.add('show');
+
         })
     } else {
         alert('Please enter a valid URL (containing https:// or http://). Thanks!');
